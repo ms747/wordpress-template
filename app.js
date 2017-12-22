cb001c1ae7a587d07e2b30565299a158b31c8542
ms747 <mayur.shah.ha@gmail.com>
2017-12-22 21:05:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("js loaded");
+
+var menu = document.getElementById('menu');
+
+
+window.onscroll = () =>{
+    console.log(window.pageYOffset);
+    if(window.pageYOffset > menu.offsetHeight){
+        menu.classList.remove('menu1');
+        menu.classList.add('menu');
+    }
+    else{
+        menu.classList.remove('menu');
+        menu.classList.add('menu1');
+    }
+};
