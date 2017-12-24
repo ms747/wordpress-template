d466a5560424b565fa54c606cdf9c8ee86c95976
ms747 <mayur@gmail.com>
2017-12-25 00:25:54 +0530

    added animations to hambuger menu using animate.css

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 console.log("js loaded");
 
-var menu = document.getElementById('menu');
-
+const menu = document.getElementById('menu');
+const hamburgermenu = document.getElementsByClassName('hamburger-menu');
 
 window.onscroll = () =>{
-    console.log(window.pageYOffset);
     if(window.pageYOffset > menu.offsetHeight){
         menu.classList.remove('menu1');
         menu.classList.add('menu');
@@ -14,4 +13,22 @@ window.onscroll = () =>{
         menu.classList.add('menu1');
     }
 };
+
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+              callback();
+            }
+        });
+        return this;
+    }
+});
+
+$(".hamburger-menu").click(function(){
+    // console.log("test");
+    $(".hamburger-menu").animateCss('pulse');
+});
 
